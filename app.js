211e601560d5c684d254119677a4bca0b7ed5eea
khanshorif331 <khanshorif331@gmail.com>
2022-02-13 22:17:13 +0600

    js file added with case calculation

---
+++ app.js
@@ -0,0 +1,21 @@
+function updateCaseNumber(isIncreasing){
+    const caseInput= document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if(isIncreasing){
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1;
+    }   
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total')
+    caseTotal.innerText = caseNumber * 59;
+}
+document.getElementById('case-plus').addEventListener('click',function(){
+    updateCaseNumber(true);
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+   updateCaseNumber(false);
+})
